92be7d852958d150a54ecfe82bbf597de455061e
Pawitra Warda <pawitrawarda@gmail.com>
2024-09-10 23:13:02 +0700

    update config

---
+++ init_lua.lua
@@ -0,0 +1,33 @@
+-- start My Own Config Neovim
+
+require 'shonenxnaifu.core.options'
+require 'shonenxnaifu.core.keymaps'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup {
+  require 'shonenxnaifu.plugins.neotree',
+  require 'shonenxnaifu.plugins.colortheme',
+  require 'shonenxnaifu.plugins.bufferline',
+  require 'shonenxnaifu.plugins.lualine',
+  require 'shonenxnaifu.plugins.treesitter',
+  require 'shonenxnaifu.plugins.telescope',
+  require 'shonenxnaifu.plugins.lsp',
+  require 'shonenxnaifu.plugins.autocompletion',
+  require 'shonenxnaifu.plugins.autoformatting',
+  require 'shonenxnaifu.plugins.gitsigns',
+  require 'shonenxnaifu.plugins.alpha',
+  require 'shonenxnaifu.plugins.indent-blankline',
+}
+
+-- end My Own Config Neovim
