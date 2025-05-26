8dbb6add679a34aaccde843564e3a23d4d5fbda2
Pawitra Warda <pawitrawarda@gmail.com>
2025-05-26 09:44:16 +0700

    update url dependency mason null and update formatter packages

--- plugins/formatting.lua
+++ plugins/formatting.lua
@@ -2,7 +2,7 @@ return {
   'nvimtools/none-ls.nvim',
   dependencies = {
     'nvimtools/none-ls-extras.nvim',
-    'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
+    'jay-babu/mason-null-ls.nvim', -- ensure dependencies are installed
   },
   config = function()
     local null_ls = require 'null-ls'
@@ -18,7 +18,7 @@ return {
         'shfmt', -- Shell formatter
         'checkmake', -- linter for Makefiles
         'ruff', -- Python linter and formatter
-        'gofmt', -- GO formatter
+        'gofumpt', -- GO formatter
       },
       automatic_installation = true,
     }
