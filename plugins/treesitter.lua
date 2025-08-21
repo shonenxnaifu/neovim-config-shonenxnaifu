069eb07696cad6ae91cbe7183d2f1f5ed41e5695
Pawitra Warda <pawitrawarda@gmail.com>
2025-08-21 11:27:10 +0700

    add markdownviewer

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    dependencies = {'OXY2DEV/markview.nvim'},
+    lazy=false,
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
